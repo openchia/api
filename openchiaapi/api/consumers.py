b479daea6d9881bd059db404eaabc449a5c43fb3
William Grzybowski <william@grzy.org>
2021-08-26 21:06:37 -0300

    Allow log rotation for log file websocket stream

--- openchiaapi/api/consumers.py
+++ openchiaapi/api/consumers.py
@@ -31,44 +31,52 @@ class LogThread(threading.Thread):
         for i in list(self._consumers):
             try:
                 i.send(text_data=data)
-            except Exception as e:
+            except Exception:
                 pass
 
     def run(self):
         global LOG_THREAD
         queue = []
-        watch_manager = pyinotify.WatchManager()
-        notifier = pyinotify.Notifier(watch_manager)
+        self.watch_manager = pyinotify.WatchManager()
+        notifier = pyinotify.Notifier(self.watch_manager)
 
-        with open(LOG_PATH, encoding='utf-8', errors='ignore') as f:
-            seek = os.stat(LOG_PATH).st_size - 2000
-            if seek > 0:
-                f.seek(seek)
-                self._last = f.read().split('\n')[-LOG_LINES:]
-                self.send(json.dumps({"data": '\n'.join(self._last)}))
+        self.fp = open(LOG_PATH, encoding='utf-8', errors='ignore')
+        seek = os.stat(LOG_PATH).st_size - 2000
+        if seek > 0:
+            self.fp.seek(seek)
+            self._last = self.fp.read().split('\n')[-LOG_LINES:]
+            self.send(json.dumps({"data": '\n'.join(self._last)}))
 
-            watch_manager.add_watch(LOG_PATH, pyinotify.IN_MODIFY, functools.partial(self._follow_callback, queue, f))
+        self.watch_manager.add_watch(
+            [LOG_PATH, os.path.dirname(LOG_PATH)],
+            pyinotify.IN_CREATE | pyinotify.IN_MODIFY,
+            functools.partial(self._follow_callback, queue),
+        )
 
-            while self._consumers:
-                notifier.process_events()
+        while self._consumers:
+            notifier.process_events()
 
-                data = "".join(queue)
-                if data:
-                    self._last += data.split('\n')
-                    self._last = self._last[-LOG_LINES:]
-                    self.send(json.dumps({"data": data}))
-                queue[:] = []
+            data = "".join(queue)
+            if data:
+                self._last += data.split('\n')
+                self._last = self._last[-LOG_LINES:]
+                self.send(json.dumps({"data": data}))
+            queue[:] = []
 
-                if notifier.check_events():
-                    notifier.read_events()
+            if notifier.check_events():
+                notifier.read_events()
 
         notifier.stop()
         LOG_THREAD = None
 
-    def _follow_callback(self, queue, f, event):
-        data = f.read()
-        if data:
-            queue.append(data)
+    def _follow_callback(self, queue, event):
+        if event.mask == pyinotify.IN_MODIFY:
+            data = self.fp.read()
+            if data:
+                queue.append(data)
+        elif event.mask == pyinotify.IN_CREATE and event.pathname == LOG_PATH:
+            self.fp.close()
+            self.fp = open(LOG_PATH, encoding='utf-8', errors='ignore')
 
 
 class PoolLogConsumer(WebsocketConsumer):
