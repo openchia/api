0948445d8071eae8e2710caf85f69e7a6277833b
William Grzybowski <william@grzy.org>
2021-09-21 08:52:28 -0300

    Add docker

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export CHIA_ROOT=/data/chia/${CHIA_NETWORK:=mainnet}
+
+trap "killall python" TERM
+
+simpleproxy -d -L 127.0.0.1:25 -R ${MAIL_HOSTNAME:=mail}:25
+
+cd /root/pool
+exec ./venv/bin/python -m pool.pool_server --log-file /data/pool_log/stdout -c /data/config.yaml
