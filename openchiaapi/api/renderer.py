e35886c67881b4440c1194c54f3185a69d553b5d
William Grzybowski <william@grzy.org>
2021-11-17 09:52:55 -0300

    Do not return raw data either

--- openchiaapi/api/renderer.py
+++ openchiaapi/api/renderer.py
@@ -5,3 +5,6 @@ class NoHTMLFormBrowsableAPIRenderer(BrowsableAPIRenderer):
 
     def get_rendered_html_form(self, *args, **kwargs):
         return ''
+
+    def get_raw_data_form(self, data, view, method, request):
+        return
