3ad29b18074ae50e3f63a2561f18c92d0ed74482
William Grzybowski <william@grzy.org>
2022-03-25 08:33:39 -0300

    Add GIVEAWAY_NEW

--- docker/start.sh
+++ docker/start.sh
@@ -14,7 +14,10 @@ if [ -n "${GIVEAWAY_ENABLED}" ]; then
 	echo "PATH=/bin:/sbin:/usr/sbin:/usr/bin" > /etc/cron.d/giveaway
 	echo "POOL_CONFIG_PATH=${POOL_CONFIG_PATH}" >> /etc/cron.d/giveaway
 	echo "DJANGO_SETTINGS_FILE=${DJANGO_SETTINGS_FILE}" >> /etc/cron.d/giveaway
-	echo "0 0 * * * root cd /root/api && (../venv/bin/python manage.py giveaway_new || true) && ../venv/bin/python manage.py giveaway_round >> /var/log/cron.log 2>&1" >> /etc/cron.d/giveaway
+	if [ -n "${GIVEAWAY_NEW}" ]; then
+		echo "0 0 * * * root cd /root/api && ../venv/bin/python manage.py giveaway_new >> /var/log/cron.log 2>&1" >> /etc/cron.d/giveaway
+	fi
+	echo "1 0 * * * root cd /root/api && ../venv/bin/python manage.py giveaway_round >> /var/log/cron.log 2>&1" >> /etc/cron.d/giveaway
 
 	cron
 fi
