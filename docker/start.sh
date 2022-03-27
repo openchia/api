7c2aa200511e858034d9990b179382e6085263e1
William Grzybowski <william@grzy.org>
2022-03-27 09:17:30 -0300

    Use tail and asyncio for logs

--- docker/start.sh
+++ docker/start.sh
@@ -3,8 +3,8 @@ set -e
 
 export CHIA_ROOT=/data/chia/${CHIA_NETWORK:=mainnet}
 export POOL_CONFIG_PATH="/data/config.yaml"
-POOL_LOG_PATH=${POOL_LOG_PATH:=/data/pool_log/stdout.json}
-export POOL_LOG_PATH
+POOL_LOG_DIR=${POOL_LOG_DIR:=/data/pool_log}
+export POOL_LOG_DIR
 
 cd /root/api
 ../venv/bin/python manage.py collectstatic --no-input
--- openchiaapi/api/consumers.py
+++ openchiaapi/api/consumers.py
@@ -1,25 +1,22 @@
-import functools
+import asyncio
 import json
 import os
-import pyinotify
-import threading
-from channels.generic.websocket import WebsocketConsumer
+import subprocess
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-LOG_PATH = os.environ.get('POOL_LOG_PATH') or '/var/log/chia/pool-server.log'
-
-LOG_THREAD = None
+LOG_DIR = os.environ.get('POOL_LOG_DIR')
+LOG_TASK = None
 LOG_LINES = 15
 
 
-class LogThread(threading.Thread):
+class LogTask(object):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
         self._consumers = []
         self._last = []
 
-    def add_consumer(self, c):
+    async def add_consumer(self, c):
         self._consumers.append(c)
         data_send = []
         for i in self._last:
@@ -27,96 +24,84 @@ class LogThread(threading.Thread):
                 data_send.append(json.loads(i))
             except ValueError:
                 data_send.append(i)
-        c.send(text_data=json.dumps({'data': data_send}))
+        await c.send(text_data=json.dumps({'data': data_send}))
 
-    def remove_consumer(self, i):
+    async def remove_consumer(self, i):
         try:
             self._consumers.remove(i)
         except Exception:
             pass
 
-    def send(self, data):
+    async def send(self, data):
         for i in list(self._consumers):
             try:
-                i.send(text_data=data)
+                await i.send(text_data=data)
             except Exception:
                 pass
 
-    def run(self):
-        global LOG_THREAD
-        queue = []
-        self.watch_manager = pyinotify.WatchManager()
-        notifier = pyinotify.Notifier(self.watch_manager)
+    async def run(self):
+        global LOG_TASK
 
-        self.fp = open(LOG_PATH, encoding='utf-8', errors='ignore')
-        seek = os.stat(LOG_PATH).st_size - 2000
-        if seek > 0:
-            self.fp.seek(seek)
-            self._last = self.fp.read().strip('\n').split('\n')[-LOG_LINES:]
-            data_send = []
-            for i in self._last:
-                try:
-                    data_send.append(json.loads(i))
-                except ValueError:
-                    data_send.append(i)
-            self.send(json.dumps({"data": data_send}))
-
-        self.watch_manager.add_watch(
-            [LOG_PATH, os.path.dirname(LOG_PATH)],
-            pyinotify.IN_CREATE | pyinotify.IN_MODIFY,
-            functools.partial(self._follow_callback, queue),
+        proc = await asyncio.create_subprocess_exec(
+            'tail',
+            '-F',
+            '-n', str(LOG_LINES),
+            os.path.join(LOG_DIR, 'main.log.json'),
+            os.path.join(LOG_DIR, 'partial.log.json'),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
         )
 
+        data_send = []
         while self._consumers:
-            notifier.process_events()
+            try:
+                line = await asyncio.wait_for(proc.stdout.readline(), 1)
+            except asyncio.TimeoutError:
+                if data_send:
+                    send = list(data_send)
+                    data_send[:] = []
+                    await self.send(json.dumps({"data": send}))
+                continue
 
-            data = "".join(queue)
-            if data:
-                data_split = data.strip('\n').split('\n')
-                self._last += data_split
-                self._last = self._last[-LOG_LINES:]
-                data_send = []
-                for i in data_split:
-                    try:
-                        data_send.append(json.loads(i))
-                    except ValueError:
-                        data_send.append(i)
-                self.send(json.dumps({"data": data_send}))
-            queue[:] = []
+            if not line:
+                break
 
-            if notifier.check_events():
-                notifier.read_events()
+            if not line.startswith(b'{'):
+                continue
 
-        notifier.stop()
-        LOG_THREAD = None
+            try:
+                data_send.append(json.loads(line.decode(errors='ignore')))
+            except ValueError:
+                pass
 
-    def _follow_callback(self, queue, event):
-        if event.mask == pyinotify.IN_MODIFY:
-            data = self.fp.read()
-            if data:
-                queue.append(data)
-        elif event.mask == pyinotify.IN_CREATE and event.pathname == LOG_PATH:
-            self.fp.close()
-            self.fp = open(LOG_PATH, encoding='utf-8', errors='ignore')
+            if len(data_send) >= 10:
+                send = list(data_send)
+                data_send[:] = []
+                await self.send(json.dumps({"data": send}))
+
+        proc.kill
+        await proc.communicate()
+
+        LOG_TASK = None
 
 
-class PoolLogConsumer(WebsocketConsumer):
-    def connect(self):
-        global LOG_THREAD
-        self.accept()
+class PoolLogConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        global LOG_TASK
+        await self.accept()
 
-        if not os.path.exists(LOG_PATH):
+        if not os.path.exists(LOG_DIR):
             return
 
-        if LOG_THREAD is None:
-            LOG_THREAD = LogThread()
-            LOG_THREAD.start()
-        LOG_THREAD.add_consumer(self)
+        if LOG_TASK is None:
+            LOG_TASK = LogTask()
+            asyncio.create_task(LOG_TASK.run())
+        await LOG_TASK.add_consumer(self)
 
-    def disconnect(self, close_code):
-        global LOG_THREAD
-        if LOG_THREAD is not None:
-            LOG_THREAD.remove_consumer(self)
+    async def disconnect(self, close_code):
+        global LOG_TASK
+        if LOG_TASK is not None:
+            LOG_TASK.remove_consumer(self)
 
-    def receive(self, text_data):
+    async def receive(self, text_data=None, bytes_data=None):
         pass
